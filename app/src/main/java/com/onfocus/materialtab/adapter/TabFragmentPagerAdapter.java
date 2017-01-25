a864425a849593ed494731c9fa9a6727ac8f547c
youngstruggle <febrian.nana@gmail.com>
2017-01-25 09:40:34 +0700

    Initial commit

---
+++ app/src/main/java/com/onfocus/materialtab/adapter/TabFragmentPagerAdapter.java
@@ -0,0 +1,50 @@
+package com.onfocus.materialtab.adapter;
+
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentPagerAdapter;
+
+import com.onfocus.materialtab.fragment.Tab1Fragment;
+import com.onfocus.materialtab.fragment.Tab2Fragment;
+
+/**
+ * Created by Nana Febriana on 24-Jan-17.
+ */
+
+public class TabFragmentPagerAdapter extends FragmentPagerAdapter {
+
+    String[] title = new String[]{
+            "Tab 1", "Tab 2"
+    };
+
+    public TabFragmentPagerAdapter(FragmentManager fm) {
+        super(fm);
+    }
+
+    @Override
+    public Fragment getItem(int position) {
+        Fragment fragment = null;
+        switch (position){
+            case 0:
+                fragment = new Tab1Fragment();
+                break;
+            case 1:
+                fragment = new Tab2Fragment();
+                break;
+            case 2:
+                fragment = null;
+                break;
+        }
+        return fragment;
+    }
+
+    @Override
+    public CharSequence getPageTitle(int position) {
+        return title[position];
+    }
+
+    @Override
+    public int getCount() {
+        return title.length;
+    }
+}
